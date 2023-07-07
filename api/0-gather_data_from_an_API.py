18c71114280d5f67dbba0d4a38d3c784e39f90df
Josuejosue1999 <j.biaou@alustudent.com>
2023-07-07 23:02:34 +0200

    Task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ def get_employee_todo_progress(employee_id):
     employee_url = f'{base_url}/users/{employee_id}'
     todos_url = f'{base_url}/todos?userId={employee_id}'
 
-    
+    # Fetch employee information
     response = requests.get(employee_url)
     if response.status_code != 200:
         print(f"Failed to retrieve employee information for ID: {employee_id}")
@@ -15,7 +15,7 @@ def get_employee_todo_progress(employee_id):
     employee_data = response.json()
     employee_name = employee_data['name']
 
-    
+    # Fetch employee's TODO list
     response = requests.get(todos_url)
     if response.status_code != 200:
         print(f"Failed to retrieve TODO list for employee: {employee_name}")
@@ -25,9 +25,10 @@ def get_employee_todo_progress(employee_id):
     total_tasks = len(todos_data)
     done_tasks = [todo['title'] for todo in todos_data if todo['completed']]
 
+    # Display TODO list progress
     print(f"Employee {employee_name} is done with tasks ({len(done_tasks)}/{total_tasks}):")
     print(f"{employee_name}: {len(done_tasks)}/{total_tasks}")
     for task in done_tasks:
         print("\t", task)
 
-
+# Example usage: get_employee_todo_progress(1)
