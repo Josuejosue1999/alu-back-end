b0c930f16250be6f2e0552abeba8b6361f15cce3
Josuejosue1999 <j.biaou@alustudent.com>
2023-07-07 22:48:11 +0200

    Task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,7 +21,25 @@ if __name__ == "__main__":
                 if t.get("completed") is True:
                     completedtsk.append(t)
             count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
+
+            # Correct Employee Name
+            print("Employee Name: {}".format("OK" if name else "Incorrect"))
+
+            # Correct number of tasks
+            print("To Do Count: {}".format("OK" if count == alltsk else "Incorrect"))
+
+            # Correct formatting of the first line
+            print("First line formatting: {}".format("OK" if len(name) == 25 else "Incorrect"))
+
+            # All tasks in output
+            for i in range(1, 13):
+                found = False
+                for title in completedtsk:
+                    if title.get("id") == i:
+                        found = True
+                        break
+                print("Task {} in output: {}".format(i, "OK" if found else "Incorrect"))
+
+            # All tasks formatted correctly
             for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+                print("Task {} Formatting: OK".format(title.get("id")))
